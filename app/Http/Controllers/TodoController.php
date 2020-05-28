fde02ce18d154c73e327ef4824ea22ccc40247f0
Aash Narayan Mahato <32462580+Narayanaash@users.noreply.github.com>
2020-05-28 20:30:11 +0530

    Update TodoController.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,1 +1,61 @@
 
+<?php
+
+namespace App\Http\Controllers;
+use App\Todo;
+use App\Http\Resources\Todo as TodoResource;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $items = Todo::all();
+
+        return TodoResource::collection($items);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'item' => 'required|max:255',
+        ]);
+
+        $todo = new Todo;
+        $todo->id = rand();
+        $todo =>item = $request->item;
+        $todo->done = fale;
+
+        if ($todo->save()) {
+            return (new TodoResource($todo))
+                ->response()
+                ->setStatusCode(201);
+        } else {
+            return response()->json(['msg' => 'error inserting'], 404);
+        }
+        return (new TodoResource($todo))
+                ->response()
+                ->setStatusCode(201);
+    }
+
+    public function update($id)
+    {
+        $item = Todo::findOrFail($id);
+        $item->done = true;
+
+        if ($item->save()) {
+            return response()->json(['msg' => 'updated'], 200);
+        } else {
+            return response()->json(['msg' => 'error updating'], 404);
+        }
+
+    }
+
+    public function delete($id)
+    {
+        $item = Todo::findOrFail($id);
+        $item->delete();
+
+        return response()->json(null, 204);
+    }
+}
